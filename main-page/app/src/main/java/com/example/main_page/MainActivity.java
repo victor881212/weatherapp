2122b68c78a705f21337945b5e8353451ec7f0fe
victor881212 <victor881212@gmail.com>
2022-10-29 14:00:26 +0800

    更新 mainpage

--- main-page/app/src/main/java/com/example/main_page/MainActivity.java
+++ main-page/app/src/main/java/com/example/main_page/MainActivity.java
@@ -5,11 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.Time;
 import android.widget.TextView;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
+
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -17,21 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Time now = new Time();
-        TextView text = (TextView) findViewById(R.id.text1);
+        TextView text = (TextView) findViewById(R.id.datetext);
         now.setToNow();
         String Time= now.toString();
         int month =now.month;
         int hour = now.hour;
-        text.setText("time"+month+'/'+hour);
-
-
-
-        String DATE_FORMAT = "yyyy-M-dd'T'hh:mm:ss.SSS'Z'";
-        DateFormat df = new SimpleDateFormat(DATE_FORMAT);
-        df.setTimeZone(TimeZone.getTimeZone("UTC"));
-        Date date = df.parse(respTime);
-        df = new SimpleDateFormat("yyyy/MM/dd HH:mm");
-        df.setTimeZone(TimeZone.getDefault());
+        text.setText("time"+month+'/'+hour); //不知道怎麼更改時區
     }
 
 
